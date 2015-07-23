78d6202d2725d7e502613f4579d8e377b6f514e7
Yazhong Liu <yorkiefixer@gmail.com>
2015-07-23 19:25:19 +0800

    change cwd to avoid searching the package.json

--- panel/page.js
+++ panel/page.js
@@ -38,6 +38,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var text = Fs.readFileSync(path, 'utf8');
   var enginePath = Path.join(__dirname, '../../engine-framework/src');
   var doc = new Firedoc({
+    cwd: enginePath,
     path: enginePath,
     parseOnly: true
   });
@@ -69,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
       default:
         if (/^(var|function)$/.test(tok.state.lastType)) break;
         if (/'[0-9]{1}'/.test(key) && tok.state.lastType === 'operator') break;
-        if (/'[a-z0-9\.]{1}'/i.test(key)) {
+        if (/'[a-z0-9\.\(\)]{1}'/i.test(key)) {
           editor.replaceRange(key[1], cur, {
             'line': cur.line,
             'xRel': cur.xRel,
